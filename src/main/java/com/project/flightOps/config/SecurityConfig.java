c1a00d1f67f1724bacc1f26ea208463e7c7d4e9b
vinay223344 <v647414@gmail.com>
2026-08-13 02:15:26 +0530

    Update SecurityConfig.java

--- src/main/java/com/project/flightOps/config/SecurityConfig.java
+++ src/main/java/com/project/flightOps/config/SecurityConfig.java
@@ -180,7 +180,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:5174","https://flightopspod7.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
